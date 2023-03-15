087c2f19c70e2caf8d3139fec860670ecf109cd2
Bebo561 <soto.carlodavid@gmail.com>
2023-03-15 13:03:42 -0400

    Gameboard for both pages designed, redirect links designed

--- src/Components/compGame.tsx
+++ src/Components/compGame.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import './compGame.css';
+import './game.css';
 import {Link} from 'react-router-dom';
 import {useState} from 'react';
 
 function CompGame(){
+    var p1Ctr = 0;
+    var compScore = 0;
     return (
         <React.Fragment>
-            <div>
-                <h1>Hello World!</h1>
-            </div>
+            <Link to="/humanGame" id = "Redirect">Versus Human</Link>
+            <h1 id ="p1">Player Wins- {p1Ctr}</h1>
+            <h1 id = "p2">Computer Wins- {compScore}</h1>
+                <div id = "game">
+                    <div className="tile right-border bottom-border" id = "1"></div>
+                    <div className="tile right-border bottom-border" id = "2"></div>
+                    <div className="tile bottom-border" id = "3"></div>
+                    <div className="tile right-border bottom-border" id = "4"></div>
+                    <div className="tile right-border bottom-border" id = "5"></div>
+                    <div className="tile bottom-border" id = "6"></div>
+                    <div className="tile right-border" id = "7"></div>
+                    <div className="tile right-border" id = "8"></div>
+                    <div className="tile" id = "9"></div>
+                    <div id="winningLine" className = "winningLine"></div>
+                </div>
+            <button id="restart">Restart</button>
         </React.Fragment>
     )
 }
--- src/Components/humanGame.tsx
+++ src/Components/humanGame.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import './humanGame.css';
+import './game.css';
 import {Link} from 'react-router-dom';
 import {useState} from 'react';
 
 function HumanGame(){
+    var p1Score = 0;
+    var p2Score = 0;
     return (
         <React.Fragment>
-            <div>
-                <h1>Hello World!</h1>
-            </div>
+            <Link to="/compGame" id = "Redirect">Versus Computer</Link>
+            <h1 id ="p1">Player 1 Wins- {p1Score}</h1>
+            <h1 id = "p2">Player 2 Wins- {p2Score}</h1>
+                <div id = "game">
+                    <div className="tile right-border bottom-border" id = "1"></div>
+                    <div className="tile right-border bottom-border" id = "2"></div>
+                    <div className="tile bottom-border" id = "3"></div>
+                    <div className="tile right-border bottom-border" id = "4"></div>
+                    <div className="tile right-border bottom-border" id = "5"></div>
+                    <div className="tile bottom-border" id = "6"></div>
+                    <div className="tile right-border" id = "7"></div>
+                    <div className="tile right-border" id = "8"></div>
+                    <div className="tile" id = "9"></div>
+                    <div id="winningLine" className = "winningLine"></div>
+                </div>
+            <button id="restart">Restart</button>
         </React.Fragment>
     )
 }
