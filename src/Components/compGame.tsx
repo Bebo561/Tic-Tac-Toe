c3abfd0492aeced8d9c890a8f6e1d8a15d930882
Bebo561 <soto.carlodavid@gmail.com>
2023-03-16 00:10:12 -0400

    Can now input into tic tac toe, change turn function works aswell as status bar

--- src/Components/compGame.tsx
+++ src/Components/compGame.tsx
@@ -4,13 +4,22 @@ import {Link} from 'react-router-dom';
 import {useState} from 'react';
 
 function CompGame(){
+
     var p1Ctr = 0;
     var compScore = 0;
+
+    var playerOne = 'X';
+    var comp = 'O';
+
+    let rand = Math.random();
+    var currTurn;
+    rand >= 0.5 ? currTurn = playerOne : currTurn = comp;
+
     return (
         <React.Fragment>
             <Link to="/humanGame" id = "Redirect">Versus Human</Link>
-            <h1 id ="p1">Player Wins- {p1Ctr}</h1>
-            <h1 id = "p2">Computer Wins- {compScore}</h1>
+            <h1 id ="p1">Player X Wins- {p1Ctr}</h1>
+            <h1 id = "p2">Computer O Wins- {compScore}</h1>
                 <div id = "game">
                     <div className="tile right-border bottom-border" id = "1"></div>
                     <div className="tile right-border bottom-border" id = "2"></div>
@@ -23,6 +32,7 @@ function CompGame(){
                     <div className="tile" id = "9"></div>
                     <div id="winningLine" className = "winningLine"></div>
                 </div>
+            <p id = "turn">Turn - {currTurn}</p>
             <button id="restart">Restart</button>
         </React.Fragment>
     )
--- src/Components/humanGame.tsx
+++ src/Components/humanGame.tsx
@@ -2,27 +2,64 @@ import React from 'react';
 import './game.css';
 import {Link} from 'react-router-dom';
 import {useState} from 'react';
+import { getElementError } from '@testing-library/react';
 
 function HumanGame(){
     var p1Score = 0;
     var p2Score = 0;
+    var playerOne = 'X';
+    var playerTwo = 'O';
+    let rand = Math.random();
+    var currTurn = '';
+    rand >= 0.5 ? currTurn = playerOne : currTurn = playerTwo;
+    //Called everytime a player turn ends
+    function ChangeTurn(){
+        if(currTurn === playerOne){
+            currTurn = playerTwo;
+            var p = document.getElementById('turn')
+            if(p != null){
+                p.innerHTML = currTurn;
+            }
+            return currTurn
+        }
+        else{
+            currTurn = playerOne;
+            var p = document.getElementById('turn')
+            if(p != null){
+                p.innerHTML = currTurn;
+            }
+            return currTurn
+        }
+    }
+
+    const handleClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
+        e.preventDefault();
+        console.log(e.currentTarget.id);
+        var cell = e.currentTarget
+        if(cell.innerHTML === ""){
+            cell = e.currentTarget
+            cell.innerHTML = currTurn;
+            ChangeTurn();
+        }
+    };
     return (
         <React.Fragment>
             <Link to="/compGame" id = "Redirect">Versus Computer</Link>
             <h1 id ="p1">Player 1 Wins- {p1Score}</h1>
             <h1 id = "p2">Player 2 Wins- {p2Score}</h1>
                 <div id = "game">
-                    <div className="tile right-border bottom-border" id = "1"></div>
-                    <div className="tile right-border bottom-border" id = "2"></div>
-                    <div className="tile bottom-border" id = "3"></div>
-                    <div className="tile right-border bottom-border" id = "4"></div>
-                    <div className="tile right-border bottom-border" id = "5"></div>
-                    <div className="tile bottom-border" id = "6"></div>
-                    <div className="tile right-border" id = "7"></div>
-                    <div className="tile right-border" id = "8"></div>
-                    <div className="tile" id = "9"></div>
+                    <div className="tile right-border bottom-border" id = "1" onClick={handleClick}></div>
+                    <div className="tile right-border bottom-border" id = "2" onClick={handleClick}></div>
+                    <div className="tile bottom-border" id = "3" onClick={handleClick}></div>
+                    <div className="tile right-border bottom-border" id = "4" onClick={handleClick}></div>
+                    <div className="tile right-border bottom-border" id = "5" onClick={handleClick}></div>
+                    <div className="tile bottom-border" id = "6" onClick={handleClick}></div>
+                    <div className="tile right-border" id = "7" onClick={handleClick}></div>
+                    <div className="tile right-border" id = "8" onClick={handleClick}></div>
+                    <div className="tile" id = "9" onClick={handleClick}></div>
                     <div id="winningLine" className = "winningLine"></div>
                 </div>
+                <p id = "turn">Turn - {currTurn}</p>
             <button id="restart">Restart</button>
         </React.Fragment>
     )
